67c065dfbfccee77df971e1179bc0c59577c4809
bejbous <125600381+bejbous@users.noreply.github.com>
2023-03-01 09:59:26 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function generate(){
+  for (var i = 0; i < 9; i++) {
+  document.write("<span class=\"sq\"></span>");
+}
+}
+
+  console.log('Jakub Holy')
---
+++ src/script.js
@@ -0,0 +1,8 @@
+function generate(){
+  for (var i = 0; i < 9; i++) {
+  document.write("<span class=\"sq\"></span>");
+}
+}
+function myfunc(){
+		document.write("Hey, here we called the JavaScript function in HTML");
+	}
